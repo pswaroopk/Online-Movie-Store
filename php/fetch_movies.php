ab0be49a672d5f25f14259cec50ef93b0d83192f
Dheeraj Goud Borlla <dheerajgoud6@gmail.com>
2017-04-22 21:30:27 -0500

    adminsupport

--- php/fetch_movies.php
+++ php/fetch_movies.php
@@ -18,9 +18,7 @@ session_start();
     $sql_connect=mysqli_connect('localhost','root','root','movie_store');
     //SELECT * FROM cart INNER JOIN movies WHERE cart.Username='swaroop' and movies.Name=cart.Name
 
-    $query = "SELECT movies.Name, Cost, Img_url
-              FROM cart INNER JOIN movies ON movies.Name=cart.Name
-              WHERE cart.Username='$user_name' AND cart.flag=1";
+    $query = "SELECT movies.Name, Cost, Img_url FROM cart INNER JOIN movies ON movies.Name=cart.Name WHERE cart.Username='$user_name'";
     $result=mysqli_query($sql_connect,$query);
     // echo $query;
     while($row=$result->fetch_assoc()){
@@ -33,14 +31,11 @@ session_start();
   }
 
   function fetchMoviesX(){
-    // $user_name = 'swaroop';
-    global $user_name;
+    $user_name = 'swaroop';
     $sql_connect=mysqli_connect('localhost','root','root','movie_store');
     //SELECT * FROM cart INNER JOIN movies WHERE cart.Username='swaroop' and movies.Name=cart.Name
 
-    $query = "SELECT movies.Name, Cost, Img_url
-              FROM cart INNER JOIN movies ON movies.Name=cart.Name
-              WHERE cart.Username='$user_name' AND cart.flag=1";
+    $query = "SELECT movies.Name, Cost, Img_url FROM cart INNER JOIN movies ON movies.Name=cart.Name WHERE cart.Username='$user_name'";
     $result=mysqli_query($sql_connect,$query);
     // echo $query;
     while($row=$result->fetch_assoc()){
